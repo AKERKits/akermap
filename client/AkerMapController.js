e3c8e7bcb6754dd1072f706789d138ceb588e322
Joscha Feth <joscha@feth.com>
2015-02-24 09:21:55 +0100

    Use Denver if we can't automatically locate the user

--- client/AkerMapController.js
+++ client/AkerMapController.js
@@ -3,25 +3,33 @@ require('./geoLocationService');
 var mapStyles = require('./map/styles/avocado.json');
 
 require('./akermap').controller('AkerMapController', function($scope, geoLocation) {
-	geoLocation().then(function(geoLocatedCoords) {
-		return {
-      center: geoLocatedCoords,
-      zoom: 12
-    };
-	}, function geoLocationFailedUseFallback() {
-    return {
-      center: {
-        latitude: 45,
-        longitude: -73
-      },
-      zoom: 9
-    };
-  }).then(function(map) {
-    $scope.map = map;
-  });
+    'use strict';
+
+	geoLocation()
+    .then(
+        function geoLocationSucceeded(geoLocatedCoords) {
+            return {
+              center: geoLocatedCoords,
+              zoom: 12
+            };
+        },
+        function geoLocationFailedUseFallback() {
+            return {
+              center: {
+                  // Denver
+                latitude: 39.7643389,
+                longitude: -104.8551114
+              },
+              zoom: 9
+            };
+        }
+    )
+    .then(function(map) {
+        $scope.map = map;
+    });
 
 
-	$scope.options = {
-		styles: mapStyles
-	};
+    $scope.options = {
+        styles: mapStyles
+    };
 });
