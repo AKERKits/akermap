b9141a9e3bf6da75d65a77905998fff2a9363437
Joscha Feth <joscha@feth.com>
2015-03-11 01:35:54 +0100

    do not use colors

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         build: {
             stats: {
                 // Configure the console output
-                colors: true,
+                colors: false,
                 modules: true,
                 reasons: false
             },
