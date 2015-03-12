2f0e367bb5631d599d6fd9d6674f850c8ebfccbc
Joscha Feth <joscha@feth.com>
2015-03-12 21:24:39 +0100

    enable gzip compression

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* global process */
 var express = require('express');
 var serveStatic = require('serve-static');
+var compress = require('compression');
 
 var app = express();
+app.use(compress());
 
 app.use(serveStatic('client', {'index': ['index.html']}));
 app.listen(process.env.PORT || 3000);
