86f3fc9005252966b2bfb704cb08d705658882a9
Joscha Feth <joscha@feth.com>
2015-03-15 18:03:34 +0100

    (chore) use spaces as indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,76 @@
 /* global __dirname */
 module.exports = (function() {
-	'use strict';
+    'use strict';
 
-	var path = require("path");
-	var webpack = require("webpack");
-	var AngularWebpackPlugin = require('angular-webpack-plugin');
-	var NgAnnotatePlugin = require('ng-annotate-webpack-plugin');
+    var path = require("path");
+    var webpack = require("webpack");
+    var AngularWebpackPlugin = require('angular-webpack-plugin');
+    var NgAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
-	var clientDir = path.resolve(__dirname, path.join(__dirname, 'client'));
+    var clientDir = path.resolve(__dirname, path.join(__dirname, 'client'));
 
-	return {
-		entry: {
-			bootstrap: [
+    return {
+        entry: {
+            bootstrap: [
                 "./client/entry.js"
             ]
-		},
-		output: {
-			path: path.join(__dirname, 'client', "assets"),
-			publicPath: "assets/",
-			filename: "[name].js",
-			chunkFilename: "[chunkhash].js"
-		},
-		module: {
-			noParse: [
-			//	/angular-translate(\/|\\)angular-translate\.js/,
-			],
-			loaders: [
-				{ test: /\.css$/, loader: "style-loader!css-loader" },
-				{ test: /\.json$/, loader: "json" },
-				{ test: /images(\/|\\).*\.svg$/, loader: "url-loader" },
-				{ test: /modernizr\.js$/, loader: 'imports?this=>window' },
-				{ test: /respond\.src\.js$/, loader: 'imports?this=>window' },
-				{ test: /ng-table\.js/, loader: 'imports?define=>null'},
-				{ test: /templates(\/|\\).*\.html$/, loader: "ngtemplate?relativeTo="+clientDir+"!html" },
-				{ test: /\.less$/, loader: "style-loader!css-loader!less-loader" }
-			]
-		},
-		resolve: {
-			root: [
-				path.resolve('bower_components')
-			],
-			alias: {
-				'pascalprecht.translate': 'angular-translate',
-				'uiGmapgoogle-maps': 'angular-google-maps/dist/angular-google-maps.js',
-				'btford.modal': "angular-modal",
-				'ngSanitize': 'angular-sanitize',
-				"ng": "angular-translate" // workaround for missing ng-module - this loads angular-translate twice :(
-			}
-		},
-		plugins: [
-			new AngularWebpackPlugin(),
-		    new webpack.ResolverPlugin(
-		        new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
-		    ),
-		    new NgAnnotatePlugin({
-		        add: true
-			})
-		]
-	};
+        },
+        output: {
+            path: path.join(__dirname, 'client', "assets"),
+            publicPath: "assets/",
+            filename: "[name].js",
+            chunkFilename: "[chunkhash].js"
+        },
+        module: {
+            noParse: [
+                //	/angular-translate(\/|\\)angular-translate\.js/,
+            ],
+            loaders: [{
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }, {
+                test: /\.json$/,
+                loader: "json"
+            }, {
+                test: /images(\/|\\).*\.svg$/,
+                loader: "url-loader"
+            }, {
+                test: /modernizr\.js$/,
+                loader: 'imports?this=>window'
+            }, {
+                test: /respond\.src\.js$/,
+                loader: 'imports?this=>window'
+            }, {
+                test: /ng-table\.js/,
+                loader: 'imports?define=>null'
+            }, {
+                test: /templates(\/|\\).*\.html$/,
+                loader: "ngtemplate?relativeTo=" + clientDir + "!html"
+            }, {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!less-loader"
+            }]
+        },
+        resolve: {
+            root: [
+                path.resolve('bower_components')
+            ],
+            alias: {
+                'pascalprecht.translate': 'angular-translate',
+                'uiGmapgoogle-maps': 'angular-google-maps/dist/angular-google-maps.js',
+                'btford.modal': "angular-modal",
+                'ngSanitize': 'angular-sanitize',
+                "ng": "angular-translate" // workaround for missing ng-module - this loads angular-translate twice :(
+            }
+        },
+        plugins: [
+            new AngularWebpackPlugin(),
+            new webpack.ResolverPlugin(
+                new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+            ),
+            new NgAnnotatePlugin({
+                add: true
+            })
+        ]
+    };
 })();
